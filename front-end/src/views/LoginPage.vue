1447eab9a536fa85aaffdf70cb734c6b1f416598
knecht <ldj910505@naver.com>
2023-05-28 20:31:15 +0900

    create front-end scaffold

---
+++ front-end/src/views/LoginPage.vue
@@ -0,0 +1,13 @@
+<!-- eslint-disable -->
+<template>
+  <div>
+    <h1>TaskAgile</h1>
+  </div>
+</template>
+<!-- eslint-disable -->
+<script>
+/* eslint-disable */
+export default {
+  name: "LoginPage",
+};
+</script>
