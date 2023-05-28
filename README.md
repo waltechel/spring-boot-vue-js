f3feea911dc00b3fd00e8175c467b2d1e5053222
knecht <ldj910505@naver.com>
2023-05-28 22:28:25 +0900

    combine build process

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # spring-boot-vue-js
+
+## 커밋
+
+- `mvn install` 하나의 명령어로 통합할 수 있다.
--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -1,4 +1,5 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-});
+export default {
+  devServer: {
+    port: 3000
+  }
+};
