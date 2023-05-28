167a498377fadd217afced82a053310abfe54b57
knecht <ldj910505@naver.com>
2023-05-28 17:36:42 +0900

    create back-end scaffold

---
+++ src/test/java/com/taskagile/SpringBootVueJsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskagile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootVueJsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
