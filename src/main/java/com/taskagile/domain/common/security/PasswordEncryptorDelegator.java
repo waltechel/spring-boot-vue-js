07a3dce5c28fe0ab1efba0e35f906313601c82c0
knecht <ldj910505@naver.com>
2023-05-29 17:43:04 +0900

    fix: invalid return value of encrypt() method
    - 일단은 비밀번호를 그대로 전달하는 것으로 작성
    - 원래는 null을 리턴했지만, 아직 스프링 시큐리티 전이므로 평문 그대로 전송하는 것으로 적용

--- src/main/java/com/taskagile/domain/common/security/PasswordEncryptorDelegator.java
+++ src/main/java/com/taskagile/domain/common/security/PasswordEncryptorDelegator.java
@@ -8,6 +8,8 @@ public class PasswordEncryptorDelegator implements PasswordEncryptor {
   @Override
   public String encrypt(String rawPassword) {
     // TODO implement this
+    // 일단은 비밀번호를 그대로 전달하는 것으로 작성
+    // 원래는 null을 리턴했지만, 아직 스프링 시큐리티 전이므로 평문 그대로 전송하는 것으로 적용
     return rawPassword;
   }
 }
